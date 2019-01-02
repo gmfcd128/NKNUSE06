b53735ae19ed09301014a3f704d177e0b3928c76
Jack Lin <gmfcd128@gmail.com>
2019-01-02 21:00:16 +0800

    使用者註冊功能新增
    目前試作出新增求職者帳號的功能

--- login.py
+++ login.py
@@ -9,10 +9,52 @@ import os
 db = dataset.connect('sqlite:///users.db')    
 
 
-def test():
-    messagebox.showerror("Oh, no", "We're FUCKED")
-
+def signup():
+    signup_window = tkinter.Toplevel(window)
+    signup_window.title("註冊新帳號")
+    signup_window.resizable(width=False, height=False)
+    signup_window.geometry('300x150')
+    frame_form = Frame(signup_window)
+    frame_radiobuttons = Frame(frame_form)
+    username_prompt = Label(frame_form , text="使用者名稱").grid(row=0, column=0, sticky=E)
+    password_prompt = Label(frame_form , text="密碼").grid(row=1, column=0, sticky=E)
+    name_prompt = Label(frame_form , text="姓名").grid(row=2, column=0, sticky=E)
+    username_entry = Entry(frame_form)
+    username_entry.grid(row=0, column=1, sticky=W)
+    password_entry = Entry(frame_form)
+    password_entry.grid(row=1, column=1, sticky=W)
+    name_entry = Entry(frame_form)
+    name_entry.grid(row=2, column=1, sticky=W)
+    usertype = tkinter.IntVar()
+    usertype_prompt = Label(frame_form, text='帳戶類型').grid(row=3, column=0, sticky=E)
+    option_employee = Radiobutton(frame_radiobuttons, text="求職者",variable=usertype, value=1).grid(row=0, column=0)
+    option_employer = Radiobutton(frame_radiobuttons, text="企業主", variable=usertype, value=2).grid(row=0, column=1)
+    frame_radiobuttons.grid(row=3, column=1)
+    register_button = Button(signup_window, text="送出", command=lambda: add_user(username_entry.get(), password_entry.get()))
+    frame_form.pack(pady=10)
+    register_button.pack()
     
+
+    def add_user(username, password):
+        for employee in db['Employees']:
+            if employee['Username'] == username:
+                messagebox.showerror("錯誤", "此帳號已被註冊")
+                return
+        for employer in db['Employers']:
+            if employer['Username'] == username:
+                messagebox.showerror("錯誤", "此帳號已被註冊")
+                return
+        if usertype.get() == 1:
+            #務必檢查該搷的欄位是否都有在註冊時搷到，不然進到主程式的時候讀到空值會錯誤。
+            db['Employees'].insert(dict(Username=username,Pw=password,
+                                        Age=0, Gender=" ", Phone=" ",
+                                        Address=" ", Education=" ",About=" "))
+        elif usertype.get() == 2:
+            db['Employers'].insert(dict(Username=username,Pw=password))
+        messagebox.showinfo("註冊成功", "您可以登入程式開始搷寫履歷!") 
+        signup_window.destroy()   
+    
+        
 def verify(username, password):
     for user in db['Employees']:
         if user['Username'] == username and user['Pw'] == password:
@@ -44,7 +86,7 @@ password_entry = Entry(frame_form)
 password_entry['show'] = '*'
 password_entry.grid(row=1, column=1, sticky=W)
 button_login = Button(frame_buttons, text="登入", command=lambda:verify(username_entry.get(), password_entry.get()))
-button_register = Button(frame_buttons, text="註冊", command=test)
+button_register = Button(frame_buttons, text="註冊", command=signup)
 button_login.pack(side=LEFT)
 button_register.pack(side=LEFT)
 frame_form.pack(pady=10)
