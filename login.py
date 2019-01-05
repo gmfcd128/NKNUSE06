afca1ff001873fec6158be52fcf09be9f9e14846
WithneyCao <45346550+WithneyCao@users.noreply.github.com>
2019-01-05 19:27:58 +0800

    修改 (判斷註冊欄位是否填寫齊全)

--- login.py
+++ login.py
@@ -36,23 +36,26 @@ def signup():
     
 
     def add_user(username, password):
-        for employee in db['Employees']:
-            if employee['Username'] == username:
-                messagebox.showerror("錯誤", "此帳號已被註冊")
-                return
-        for employer in db['Employers']:
-            if employer['Username'] == username:
-                messagebox.showerror("錯誤", "此帳號已被註冊")
-                return
-        if usertype.get() == 1:
-            #務必檢查該搷的欄位是否都有在註冊時搷到，不然進到主程式的時候讀到空值會錯誤。
-            db['Employees'].insert(dict(Username=username,Pw=password,
-                                        Age=0, Gender=" ", Phone=" ",
-                                        Address=" ", Education=" ",About=" "))
-        elif usertype.get() == 2:
-            db['Employers'].insert(dict(Username=username,Pw=password))
-        messagebox.showinfo("註冊成功", "您可以登入程式開始搷寫履歷!") 
-        signup_window.destroy()   
+        if username!="" and password!="" and name!="" and usertype!="":
+            for employee in db['Employees']:
+                if employee['Username'] == username:
+                    messagebox.showerror("錯誤", "此帳號已被註冊")
+                    return
+            for employer in db['Employers']:
+                if employer['Username'] == username:
+                    messagebox.showerror("錯誤", "此帳號已被註冊")
+                    return
+            if usertype.get() == 1:
+                #務必檢查該搷的欄位是否都有在註冊時填到，不然進到主程式的時候讀到空值會錯誤。
+                db['Employees'].insert(dict(Username=username,Pw=password,
+                                            Age=0, Gender=" ", Phone=" ",
+                                            Address=" ", Education=" ",About=" "))
+            elif usertype.get() == 2:    
+                db['Employers'].insert(dict(Username=username,Pw=password))
+            messagebox.showinfo("註冊成功", "您可以登入程式開始搷寫履歷!") 
+            signup_window.destroy()   
+        else:
+            messagebox.showinfo("錯誤","有欄位沒有填寫")
     
         
 def verify(username, password):
@@ -93,4 +96,5 @@ frame_form.pack(pady=10)
 frame_buttons.pack()
 
 window.mainloop()
+
 
