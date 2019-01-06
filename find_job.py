41f5cd15a128ba7cf9c892795eef8fa6ea51dad7
Jack Lin <gmfcd128@gmail.com>
2019-01-07 05:18:02 +0800

    微幅介面優化

--- find_job.py
+++ find_job.py
@@ -8,183 +8,180 @@ from tkinter import *
 import dataset
 import sys
 
-db = dataset.connect('sqlite:///users.db')
-jobs_db = dataset.connect('sqlite:///jobs.db') 
-user_id = 0
 
-
-def test():
-    messagebox.showinfo("Info", "We're FUCKED.")
-
-
-window2 = tk.Tk() 
-user_id = int(sys.argv[1])        
-current_user = db['Employees'].find_one(ID=user_id)             
-window2.title("E04求職網-求職者視角" + "(目前使用者：" + current_user['Name'] + ")")
-window2.resizable(width=False, height=False)
-window2.geometry('800x600')        
-tabControl = ttk.Notebook(window2)          
-
-# 視窗標籤相關設定
-tab1 = ttk.Frame(tabControl)  
-tab2 = ttk.Frame(tabControl)
-tab3 = ttk.Frame(tabControl)           
-tabControl.add(tab1, text="職缺搜尋") 
-tabControl.add(tab2, text="回覆狀況") 
-tabControl.add(tab3, text="履歷編輯")
-tabControl.pack(expand=1, fill="both")
-
-selected_job = None
-
-
-def select_cmd(selected):
-    print ('Selected item ID:', int(list_jobs.get(list_jobs.curselection()[0])[0][0]))
+def gui(id):
+    db = dataset.connect('sqlite:///users.db')
+    jobs_db = dataset.connect('sqlite:///jobs.db') 
+    user_id = id
+    
+    def test():
+        messagebox.showinfo("Info", "We're FUCKED.")
+    
+    window2 = tk.Tk()         
+    current_user = db['Employees'].find_one(ID=user_id)             
+    window2.title("E04求職網-求職者視角" + "(目前使用者：" + current_user['Name'] + ")")
+    window2.resizable(width=False, height=False)
+    window2.geometry('800x600')        
+    tabControl = ttk.Notebook(window2)          
+    
+    # 視窗標籤相關設定
+    tab1 = ttk.Frame(tabControl)  
+    tab2 = ttk.Frame(tabControl)
+    tab3 = ttk.Frame(tabControl)           
+    tabControl.add(tab1, text="職缺搜尋") 
+    tabControl.add(tab2, text="回覆狀況") 
+    tabControl.add(tab3, text="履歷編輯")
+    tabControl.pack(expand=1, fill="both")
+    
     global selected_job
-    selected_job = jobs_db['Jobs'].find_one(ID=list_jobs.get(list_jobs.curselection()[0])[0][0])
-    print(selected_job['ID'])
-
-
-def show_detail(job):
-    company = db['Employer'].find_one(ID=job['CompanyID'])
-    window_details = tk.Toplevel(window2)
-    window_details.title("職缺詳細資料")
-    window_details.geometry('300x500')
-    Grid.columnconfigure(window_details, 1, weight=1)
     
-    label_company = Label(window_details, text="發布公司").grid(row=0, column=0)
-    text_company = Entry(window_details)
-    text_company.insert(END, company['companyName'])
-    text_company.config(state='readonly')
-    text_company.grid(row=0, column=1, sticky=N + E + W)
+    def select_cmd(selected):
+        print ('Selected item ID:', int(list_jobs.get(list_jobs.curselection()[0])[0][0]))
+        global selected_job
+        selected_job = jobs_db['Jobs'].find_one(ID=list_jobs.get(list_jobs.curselection()[0])[0][0])
+        print(selected_job['ID'])
     
-    label_title = Label(window_details, text="工作名稱").grid(row=1, column=0)
-    text_title = Entry(window_details)
-    text_title.insert(END, job['Name'])
-    text_title.config(state='readonly')
-    text_title.grid(row=1, column=1, sticky=N + E + W)
+    def show_detail(job):
+        company = db['Employer'].find_one(ID=job['CompanyID'])
+        window_details = tk.Toplevel(window2)
+        window_details.title("職缺詳細資料")
+        window_details.geometry('300x500')
+        window_details.resizable(width=False, height=False)
+        Grid.columnconfigure(window_details, 1, weight=1)
+        
+        label_company = Label(window_details, text="發布公司").grid(row=0, column=0)
+        text_company = Entry(window_details)
+        text_company.insert(END, company['companyName'])
+        text_company.config(state='readonly')
+        text_company.grid(row=0, column=1, sticky=N + E + W)
+        
+        label_title = Label(window_details, text="工作名稱").grid(row=1, column=0)
+        text_title = Entry(window_details)
+        text_title.insert(END, job['Name'])
+        text_title.config(state='readonly')
+        text_title.grid(row=1, column=1, sticky=N + E + W)
+        
+        label_content = Label(window_details, text="工作內容").grid(row=2, column=0, sticky=N)
+        text_content = ScrolledText(window_details)
+        text_content.insert(END, job['Content'])
+        text_content.config(state='disabled')
+        text_content.grid(row=2, column=1, sticky=N + S + E + W)
+        Grid.rowconfigure(window_details, 2, weight=1)
+        
+        label_requirement = Label(window_details, text="工作要求").grid(row=3, column=0, sticky=N)
+        text_requirement = ScrolledText(window_details)
+        text_requirement.insert(END, job['Requirement'])
+        text_requirement.config(state='disabled')
+        text_requirement.grid(row=3, column=1, sticky=N + S + E + W)
+        Grid.rowconfigure(window_details, 3, weight=1)
+        
+        label_requirement = Label(window_details, text="聯絡方式").grid(row=4, column=0, sticky=N)
+        text_requirement = ScrolledText(window_details)
+        text_requirement.insert(END, job['Contact'])
+        text_requirement.config(state='disabled')
+        text_requirement.grid(row=4, column=1, sticky=N + S + E + W)
+        Grid.rowconfigure(window_details, 4, weight=1)
+        
+        frame_button = Frame(window_details)
+        button_confirm = Button(frame_button, text="送出履歷", command=lambda:send_resume(job))
+        button_confirm.pack(pady=5)
+        frame_button.grid(row=5, column=0, columnspan=2)
+        
+        def send_resume(job):
+            MsgBox = tk.messagebox.askquestion ('即將送出資料', '你確定要應徵這份工作嗎?', icon='warning')
+            if MsgBox == 'yes':
+                print(selected_job['ID'])
+                print(current_user['ID'])
+                jobs_db['Replies'].insert(dict(RecipientID=current_user['ID'], JobID=selected_job['ID'], Stat=0))
+                window_details.destroy()
+            else:
+                window_details.destroy()
     
-    label_content = Label(window_details, text="工作內容").grid(row=2, column=0, sticky=N)
-    text_content = ScrolledText(window_details)
-    text_content.insert(END, job['Content'])
-    text_content.config(state='disabled')
-    text_content.grid(row=2, column=1, sticky=N + S + E + W)
-    Grid.rowconfigure(window_details, 2, weight=1)
+    # 職缺列表
+    list_jobs = treectrl.MultiListbox(tab1)
+    list_jobs.pack(expand=1, fill="both")
+    list_jobs.focus_set()   
+    list_jobs.configure(selectcmd=select_cmd, selectmode='extended')
+    list_jobs.config(columns=('ID', '名稱', '發布公司', "待遇", "缺額", '聯絡資訊'))
     
-    label_requirement = Label(window_details, text="工作要求").grid(row=3, column=0, sticky=N)
-    text_requirement = ScrolledText(window_details)
-    text_requirement.insert(END, job['Requirement'])
-    text_requirement.config(state='disabled')
-    text_requirement.grid(row=3, column=1, sticky=N + S + E + W)
-    Grid.rowconfigure(window_details, 3, weight=1)
+    for job in jobs_db['Jobs']:
+        # 因應python預設不支援多行文字 在時間不足以重寫UI下對多行文字進行格式化
+        contact = job['Contact'].splitlines()
+        contact_display = "/".join(contact)
+        company = db['Employer'].find_one(ID=job['CompanyID'])
+        list_jobs.insert(END, job['ID'], job['Name'], company['companyName'], job['Salary'], job['Vacancy'], contact_display)
+        print(job['Contact'])
     
-    label_requirement = Label(window_details, text="聯絡方式").grid(row=4, column=0, sticky=N)
-    text_requirement = ScrolledText(window_details)
-    text_requirement.insert(END, job['Contact'])
-    text_requirement.config(state='disabled')
-    text_requirement.grid(row=4, column=1, sticky=N + S + E + W)
-    Grid.rowconfigure(window_details, 4, weight=1)
+    button_apply = Button(tab1, text='應徵', command=lambda:show_detail(selected_job))
+    button_apply.pack()
     
-    button_confirm = Button(window_details, text="送出履歷", command=lambda:send_resume(job))
-    button_confirm.grid(row=5, column=0, sticky=W)
+    # 求職狀態
+    list_replies = treectrl.MultiListbox(tab2)
+    list_replies.pack(expand=1, fill="both")
+    list_replies.focus_set()
+    list_replies.configure(columns=('職位', '公司', '結果'))
+    for result in jobs_db['Replies']:
+        job = jobs_db['Jobs'].find_one(ID=result['JobID'])
+        company = db['Employer'].find_one(ID=job['CompanyID'])
+        result_display = ""
+        if result['Stat'] == 0:
+            result_display = "企業尚未看過"
+        elif result['Stat'] == 1:
+            result_display = "請依約定日期前往面試"
+        elif result['Stat'] == 2:
+            result_display = "謝謝再聯絡"
+        list_replies.insert(END, company['companyName'], job['Name'], result_display)
     
-    def send_resume(job):
-        MsgBox = tk.messagebox.askquestion ('即將送出資料', '你確定要應徵這份工作嗎?', icon='warning')
-        if MsgBox == 'yes':
-            print(selected_job['ID'])
-            print(current_user['ID'])
-            jobs_db['Replies'].insert(dict(RecipientID=current_user['ID'], JobID=selected_job['ID'], Stat=0))
-            window_details.destroy()
-        else:
-            window_details.destroy()
-
-
-# 職缺列表
-list_jobs = treectrl.MultiListbox(tab1)
-list_jobs.pack(expand=1, fill="both")
-list_jobs.focus_set()   
-list_jobs.configure(selectcmd=select_cmd, selectmode='extended')
-list_jobs.config(columns=('ID', '名稱', '發布公司', "待遇", "缺額", '聯絡資訊'))
-
-for job in jobs_db['Jobs']:
-    # 因應python預設不支援多行文字 在時間不足以重寫UI下對多行文字進行格式化
-    contact = job['Contact'].splitlines()
-    contact_display = "/".join(contact)
-    company = db['Employer'].find_one(ID=job['CompanyID'])
-    list_jobs.insert(END, job['ID'], job['Name'], company['companyName'], job['Salary'], job['Vacancy'], contact_display)
-    print(job['Contact'])
-
-button_apply = Button(tab1, text='應徵', command=lambda:show_detail(selected_job))
-button_apply.pack()
-
-# 求職狀態
-list_replies = treectrl.MultiListbox(tab2)
-list_replies.pack(expand=1, fill="both")
-list_replies.focus_set()
-list_replies.configure(columns=('職位', '公司', '結果'))
-for result in jobs_db['Replies']:
-    job = jobs_db['Jobs'].find_one(ID=result['JobID'])
-    company = db['Employer'].find_one(ID=job['CompanyID'])
-    result_display = ""
-    if result['Stat'] == 0:
-        result_display = "企業尚未看過"
-    elif result['Stat'] == 1:
-        result_display = "請依約定日期前往面試"
-    elif result['Stat'] == 2:
-        result_display = "謝謝再聯絡"
-    list_replies.insert(END, company['companyName'], job['Name'], result_display)
-
-
-# 按下更新按鈕後執行以下sql命令更新資料庫
-def update_info():
-    db.query('UPDATE Employees SET Name = "' + name_entry.get() + '" WHERE ID=' + str(user_id) + ';')
-    db.query('UPDATE Employees SET Age = "' + age_entry.get() + '" WHERE ID=' + str(user_id) + ';')
-    db.query('UPDATE Employees SET Gender = "' + gender_entry.get() + '" WHERE ID=' + str(user_id) + ';')
-    db.query('UPDATE Employees SET Phone = "' + phone_entry.get() + '" WHERE ID=' + str(user_id) + ';')
-    db.query('UPDATE Employees SET Address = "' + address_entry.get() + '" WHERE ID=' + str(user_id) + ';')
-    db.query('UPDATE Employees SET Education = "' + education_entry.get() + '" WHERE ID=' + str(user_id) + ';')
-    db.query('UPDATE Employees SET About = "' + extra_entry.get(1.0, END) + '" WHERE ID=' + str(user_id) + ';')
-
-
-# 資料編輯頁面
-Grid.columnconfigure(tab3, 1, weight=1)
-name_prompt = Label(tab3, text="姓名").grid(row=0, column=0, sticky=N + W)
-name_entry = Entry(tab3)
-name_entry.grid(row=0, column=1, sticky=E + W)
-name_entry.insert(0, current_user['Name'])
-
-gender_prompt = Label(tab3, text="性別").grid(row=1, column=0, sticky=N + W)
-gender_entry = Entry(tab3)
-gender_entry.grid(row=1, column=1, sticky=E + W)
-gender_entry.insert(0, current_user['Gender'])
-
-age_prompt = Label(tab3, text="年齡").grid(row=2, column=0, sticky=N + W)
-age_entry = Entry(tab3)
-age_entry.grid(row=2, column=1, sticky=E + W)
-age_entry.insert(0, current_user['Age'])
-
-address_prompt = Label(tab3, text="地址").grid(row=3, column=0, sticky=N + W)
-address_entry = Entry(tab3)
-address_entry.grid(row=3, column=1, sticky=E + W)
-address_entry.insert(0, current_user['Address'])
-
-phone_prompt = Label(tab3, text="電話").grid(row=4, column=0, sticky=N + W)
-phone_entry = Entry(tab3)
-phone_entry.grid(row=4, column=1, sticky=E + W)
-phone_entry.insert(0, current_user['Phone'])
-
-education_prompt = Label(tab3, text="教育程度").grid(row=5, column=0, sticky=N + W)
-education_entry = Entry(tab3)
-education_entry.grid(row=5, column=1, sticky=E + W)
-education_entry.insert(0, current_user['Education'])
-Grid.rowconfigure(tab3, 6, weight=1)
-extra_prompt = Label(tab3, text="自我介紹").grid(row=6, column=0, sticky=N + W)
-extra_entry = ScrolledText(tab3)
-extra_entry.grid(row=6, column=1, sticky=N + W + E + S)
-extra_entry.insert(INSERT, current_user['About'])
-save_button = Button(tab3, text="更新內容", command=update_info)
-save_button.grid(row=7, column=1)
-
-# 畫面出現
-
-window2.mainloop() 
-                             
+    # 按下更新按鈕後執行以下sql命令更新資料庫
+    def update_info():
+        db.query('UPDATE Employees SET Name = "' + name_entry.get() + '" WHERE ID=' + str(user_id) + ';')
+        db.query('UPDATE Employees SET Age = "' + age_entry.get() + '" WHERE ID=' + str(user_id) + ';')
+        db.query('UPDATE Employees SET Gender = "' + gender_entry.get() + '" WHERE ID=' + str(user_id) + ';')
+        db.query('UPDATE Employees SET Phone = "' + phone_entry.get() + '" WHERE ID=' + str(user_id) + ';')
+        db.query('UPDATE Employees SET Address = "' + address_entry.get() + '" WHERE ID=' + str(user_id) + ';')
+        db.query('UPDATE Employees SET Education = "' + education_entry.get() + '" WHERE ID=' + str(user_id) + ';')
+        db.query('UPDATE Employees SET About = "' + extra_entry.get(1.0, END) + '" WHERE ID=' + str(user_id) + ';')
+    
+    # 資料編輯頁面
+    Grid.columnconfigure(tab3, 1, weight=1)
+    name_prompt = Label(tab3, text="姓名").grid(row=0, column=0, sticky=N + W)
+    name_entry = Entry(tab3)
+    name_entry.grid(row=0, column=1, sticky=E + W)
+    name_entry.insert(0, current_user['Name'])
+    
+    gender_prompt = Label(tab3, text="性別").grid(row=1, column=0, sticky=N + W)
+    gender_entry = Entry(tab3)
+    gender_entry.grid(row=1, column=1, sticky=E + W)
+    gender_entry.insert(0, current_user['Gender'])
+    
+    age_prompt = Label(tab3, text="年齡").grid(row=2, column=0, sticky=N + W)
+    age_entry = Entry(tab3)
+    age_entry.grid(row=2, column=1, sticky=E + W)
+    age_entry.insert(0, current_user['Age'])
+    
+    address_prompt = Label(tab3, text="地址").grid(row=3, column=0, sticky=N + W)
+    address_entry = Entry(tab3)
+    address_entry.grid(row=3, column=1, sticky=E + W)
+    address_entry.insert(0, current_user['Address'])
+    
+    phone_prompt = Label(tab3, text="電話").grid(row=4, column=0, sticky=N + W)
+    phone_entry = Entry(tab3)
+    phone_entry.grid(row=4, column=1, sticky=E + W)
+    phone_entry.insert(0, current_user['Phone'])
+    
+    education_prompt = Label(tab3, text="教育程度").grid(row=5, column=0, sticky=N + W)
+    education_entry = Entry(tab3)
+    education_entry.grid(row=5, column=1, sticky=E + W)
+    education_entry.insert(0, current_user['Education'])
+    Grid.rowconfigure(tab3, 6, weight=1)
+    extra_prompt = Label(tab3, text="自我介紹").grid(row=6, column=0, sticky=N + W)
+    extra_entry = ScrolledText(tab3)
+    extra_entry.grid(row=6, column=1, sticky=N + W + E + S)
+    extra_entry.insert(INSERT, current_user['About'])
+    save_button = Button(tab3, text="更新內容", command=update_info)
+    save_button.grid(row=7, column=1)
+    
+    # 畫面出現
+    
+    window2.mainloop() 
+                                 
--- login.py
+++ login.py
@@ -4,6 +4,8 @@ from tkinter import messagebox
 import tkinter
 import dataset
 import os
+import find_job
+import employer
 
 # 將登入器接上員工的資料庫    
 db = dataset.connect('sqlite:///users.db')    
@@ -27,17 +29,15 @@ def signup():
     name_entry.grid(row=2, column=1, sticky=W)
     usertype = tkinter.IntVar()
     usertype_prompt = Label(frame_form, text='帳戶類型').grid(row=3, column=0, sticky=E)
-    option_employee = Radiobutton(frame_radiobuttons, text="求職者",variable=usertype, value=1).grid(row=0, column=0)
+    option_employee = Radiobutton(frame_radiobuttons, text="求職者", variable=usertype, value=1).grid(row=0, column=0)
     option_employer = Radiobutton(frame_radiobuttons, text="企業主", variable=usertype, value=2).grid(row=0, column=1)
     frame_radiobuttons.grid(row=3, column=1)
     register_button = Button(signup_window, text="送出", command=lambda: add_user(username_entry.get(), password_entry.get(), name_entry.get()))
     frame_form.pack(pady=10)
     register_button.pack()
 
-    
-
     def add_user(username, password, name):
-        if username!="" and password!=""  and name!="" and usertype.get()!=0:
+        if username != "" and password != ""  and name != "" and usertype.get() != 0:
             for employee in db['Employees']:
                 if employee['Username'] == username:
                     messagebox.showerror("註冊失敗", "此帳號已被註冊")
@@ -47,17 +47,17 @@ def signup():
                     messagebox.showerror("註冊失敗", "此帳號已被註冊")
                     return
             if usertype.get() == 1:
-                #務必檢查該搷的欄位是否都有在註冊時填到，不然進到主程式的時候讀到空值會錯誤。
+                # 務必檢查該搷的欄位是否都有在註冊時填到，不然進到主程式的時候讀到空值會錯誤。
                 db['Employees'].insert(dict(Name=name, Username=username, Pw=password,
                                             Age=0, Gender=" ", Phone=" ",
-                                            Address=" ", Education=" ",About=" "))
+                                            Address=" ", Education=" ", About=" "))
             elif usertype.get() == 2:    
-                db['Employers'].insert(dict(userName=username,passWord=password, companyIntro=" ",
+                db['Employers'].insert(dict(userName=username, passWord=password, companyIntro=" ",
                                             companyName=" ", companyDes=" "))
             messagebox.showinfo("註冊成功", "您可以登入程式開始搷寫履歷!") 
             signup_window.destroy()   
         else:
-            messagebox.showerror("註冊失敗","有欄位沒有填寫")
+            messagebox.showerror("註冊失敗", "有欄位沒有填寫")
     
         
 def verify(username, password):
@@ -65,13 +65,13 @@ def verify(username, password):
         if user['Username'] == username and user['Pw'] == password:
             messagebox.showinfo("登入成功", "即將進入求職者視角")
             window.destroy()
-            os.system("python find_job.py %d" % (user['ID']))
+            find_job.gui(user['ID'])
             return
     for company in db['Employer']:
         if company['userName'] == username and company['passWord'] == password:
             messagebox.showinfo("登入成功", "即將進入公司方視角")
             window.destroy()
-            os.system("python employer.py %d" % (company['ID']))
+            employer.gui(company['ID'])
             return
     messagebox.showerror("登入失敗", "帳號或密碼輸入錯誤")
 
@@ -83,10 +83,10 @@ window.title("E04求職登入器")
 window.resizable(width=False, height=False)
 window.geometry('450x250')
 
-#插入圖片
+# 插入圖片
 window.canvas = tkinter.Canvas(height=160, width=400)
-window.image_file = tkinter.PhotoImage(file='get_a_job.png')#匯入圖檔
-window.image = window.canvas.create_image(0,0, anchor='nw', image=window.image_file)
+window.image_file = tkinter.PhotoImage(file='get_a_job.png')  # 匯入圖檔
+window.image = window.canvas.create_image(0, 0, anchor='nw', image=window.image_file)
 window.canvas.pack(side='bottom')
 
 username_prompt = Label(frame_form , text="使用者名稱").grid(row=0, column=0, sticky=E)
